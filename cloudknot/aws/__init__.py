b5a7afba366ef8b2b054f9bd8f88a7aa408382e1
Adam Richie-Halford <richford@uw.edu>
2017-09-22 18:14:44 -0700

    Add CannotDeleteResourceException to import in aws/__init__.py

--- cloudknot/aws/__init__.py
+++ cloudknot/aws/__init__.py
@@ -2,4 +2,6 @@ from .iam import *  # noqa
 from .ecr import *  # noqa
 from .ec2 import *  # noqa
 from .batch import *  # noqa
-from .base_classes import ResourceExistsException, ResourceDoesNotExistException  # noqa
+from .base_classes import ResourceExistsException, \
+    ResourceDoesNotExistException,  \
+    CannotDeleteResourceException  # noqa
