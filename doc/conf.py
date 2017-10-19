05579e27b9f3322245b3ee84ed6a096a270286d6
Adam Richie-Halford <richford@uw.edu>
2017-10-19 14:32:35 -0700

    Use napolean instead of numpydoc in doc/conf.py. Switch html themes in doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -32,12 +32,12 @@ sys.path.append(os.path.join(currentdir, 'tools'))
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.0'  # numpydoc requires sphinc >= 1.0
+needs_sphinx = '1.0'  # numpydoc requires sphinx >= 1.0
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -52,10 +52,8 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.ifconfig',
               'sphinx.ext.autosummary',
               'sphinx.ext.mathjax',
-              'math_dollar',  # has to go before numpydoc
-              'numpydoc',
-              'github',
-              'sphinx_gallery.gen_gallery']
+              'sphinx.ext.napoleon',
+              'github']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -82,8 +80,13 @@ sphinx_gallery_conf = {
 }
 
 # Automatically generate stub pages for API
-autosummary_generate = True
+autoclass_content = "both"  # include both class docstring and __init__
 autodoc_default_flags = ['members', 'inherited-members']
+autosummary_generate = True
+
+# Napoleon settings (other than default)
+napoleon_google_docstring = False
+napoleon_use_rtype = False
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -137,7 +140,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
