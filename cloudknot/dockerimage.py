e3a3a1a8ab642c519d134cce816d56fe4b92dd80
Adam Richie-Halford <richford@uw.edu>
2017-11-05 07:24:33 -0800

    Write from header.py.txt in DockerImage._write_script() instead of hard coding f.write statements

--- cloudknot/dockerimage.py
+++ cloudknot/dockerimage.py
@@ -339,39 +339,19 @@ class DockerImage(aws.NamedObject):
         Use clize.run to create CLI
         """
         with open(self.script_path, 'w') as f:
-            # i is an indentation shorthand
-            i = '    '
-            i2 = i + i
-            f.write('import boto3\n')
-            f.write('import cloudpickle\n')
-            f.write('import os\n')
-            f.write('from clize import run\n')
-            f.write('from functools import wraps\n\n\n')
-            f.write('def pickle_to_s3(f):\n')
-            f.write(i + '@wraps(f)\n')
-            f.write(i + 'def wrapper(*args, **kwargs):\n')
-            f.write(i2 + 's3 = boto3.client("s3")\n')
-            f.write(i2 + 'bucket = '
-                         'os.environ.get("CLOUDKNOT_JOBS_S3_BUCKET")\n')
-            f.write(i2 + 'key = "cloudknot.jobs/"\n')
-            f.write(i2 + 'key += '
-                         'os.environ.get("CLOUDKNOT_S3_JOBDEF_KEY")\n')
-            f.write(i2 + 'key += "/" + '
-                         'os.environ.get("AWS_BATCH_JOB_ID")\n')
-            f.write(i2 + 'key += '
-                         '"/" + os.environ.get("AWS_BATCH_JOB_ATTEMPT")\n')
-            f.write(i2 + 'key += '
-                         '"/output.pickle"\n')
-            f.write(i2 + 'pickled_result = '
-                         'cloudpickle.dumps(f(*args, **kwargs))\n')
-            f.write(i2 + 's3.put_object(Bucket=bucket, '
-                         'Body=pickled_result, Key=key)\n\n')
-            f.write(i + 'return wrapper\n\n\n')
-            f.write('@pickle_to_s3\n')
+            header_path = os.path.abspath(os.path.join(
+                os.path.dirname(__file__),
+                'header.py.txt'
+            ))
+
+            with open(header_path) as header:
+                header_lines = header.readlines()
+                f.writelines(header_lines)
+
             f.write(inspect.getsource(self.func))
             f.write('\n\n')
             f.write('if __name__ == "__main__":\n')
-            f.write(i + 'run({func_name:s})\n'.format(func_name=self.name))
+            f.write('    run({func_name:s})\n'.format(func_name=self.name))
 
         mod_logger.info(
             'Wrote python function {func:s} to script {script:s}'.format(
