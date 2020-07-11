553aba07cab3d2a0005a5c1f5413baeebf70829b
Adam Richie-Halford <richford@uw.edu>
2020-07-11 04:56:35 -0700

    Update moto version

--- cloudknot/version.py
+++ cloudknot/version.py
@@ -95,7 +95,7 @@ EXTRAS_REQUIRE = {
         "pytest>=3.6",
         "pytest-cov",
         "flake8",
-        "moto @ git+ssh://git@github.com/spulec/moto@master",
+        "moto>=1.3.15.dev959",
     ],
 }
 ENTRY_POINTS = {"console_scripts": ["cloudknot=cloudknot.cli:main"]}
