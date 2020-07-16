4b7189031e6b6f67424f778004aea18cae119deb
John K <jk23@att.net>
2020-07-16 12:06:15 -0700

    add is_shell to new call in dockerimage

--- cloudknot/dockerimage.py
+++ cloudknot/dockerimage.py
@@ -691,7 +691,7 @@ class DockerImage(aws.NamedObject):
         else:
             # Then we're actually doing this thing. Use the Docker CLI
             # Refresh the aws ecr login credentials
-            login_cmd = subprocess.check_output(cmd)
+            login_cmd = subprocess.check_output(cmd, shell=is_windows)
 
             # Login
             login_cmd_list = login_cmd.decode("ASCII").rstrip("\n").split(" ")
