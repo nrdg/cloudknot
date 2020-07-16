978e3458d573f1e8fd967e47aa5bc9f7fa6e1f35
Adam Richie-Halford <richford@uw.edu>
2020-07-16 16:56:11 -0700

    Add pre-commit to dev install

--- cloudknot/version.py
+++ cloudknot/version.py
@@ -98,6 +98,6 @@ REQUIRES = [
 ]
 EXTRAS_REQUIRE = {
     ':python_version < "3.0"': ["configparser"],
-    "dev": ["pytest>=3.6", "pytest-cov", "flake8", "moto>=1.3.15.dev964"],
+    "dev": ["pytest>=3.6", "pytest-cov", "flake8", "moto>=1.3.15.dev964", "pre-commit"],
 }
 ENTRY_POINTS = {"console_scripts": ["cloudknot=cloudknot.cli:main"]}
