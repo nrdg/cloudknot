17bc9e75d68315aef2cb5818cdd44c0c53e93f75
Adam Richie-Halford <richford@uw.edu>
2020-07-20 06:17:46 -0700

    Get version number programmatically using importlib.metadata in cloudknot/__init__.py'

--- cloudknot/__init__.py
+++ cloudknot/__init__.py
@@ -14,7 +14,13 @@ from .aws.base_classes import get_s3_params, set_s3_params  # noqa
 from .aws.base_classes import refresh_clients  # noqa
 from .cloudknot import *  # noqa
 from .dockerimage import *  # noqa
-from .version import version as __version__  # noqa
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # package is not installed
+    pass
 
 try:
     fnull = open(os.devnull, "w")
