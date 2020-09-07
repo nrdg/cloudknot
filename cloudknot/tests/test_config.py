bfb173a8316a88d75dfe26b0fdde8d3aa79d8e2f
Adam Richie-Halford <richiehalford@gmail.com>
2020-09-07 16:27:14 -0700

    Use nosec comments for fake AWS credentials

--- cloudknot/tests/test_config.py
+++ cloudknot/tests/test_config.py
@@ -28,9 +28,9 @@ def composed(*decs):
 def aws_credentials():
     """Mocked AWS Credentials for moto."""
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
-    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
-    os.environ["AWS_SECURITY_TOKEN"] = "testing"
-    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"  # nosec
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"  # nosec
+    os.environ["AWS_SESSION_TOKEN"] = "testing"  # nosec
 
 
 mock_all = composed(
