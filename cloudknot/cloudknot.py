a9f67185c7e6a356feb73d12cadf05476c2fa06e
Adam Richie-Halford <richiehalford@gmail.com>
2021-04-23 16:33:13 -0700

    Use boto3 to get valid ec2 instance types

--- cloudknot/cloudknot.py
+++ cloudknot/cloudknot.py
@@ -26,6 +26,17 @@ def _stack_out(key, outputs):
     return o["OutputValue"]
 
 
+def _ec2_instance_types():
+    """Yield all available EC2 instance types"""
+    paginator = aws.clients["ec2"].get_paginator("describe_instance_types")
+    response_iterator = paginator.paginate()
+    responses = [
+        lst for sublist in response_iterator for lst in sublist["InstanceTypes"]
+    ]
+    instance_types = [inst["InstanceType"] for inst in responses]
+    return set(instance_types)
+
+
 # noinspection PyPropertyAccess,PyAttributeOutsideInit
 class Pars(aws.NamedObject):
     """
@@ -1165,162 +1176,7 @@ class Knot(aws.NamedObject):
                 )
 
             # Validate instance types
-            valid_instance_types = {
-                "optimal",
-                # Current generation general purpose
-                "t2",
-                "m4",
-                "m5",
-                "m5d",
-                "t2.nano",
-                "t2.micro",
-                "t2.small",
-                "t2.medium",
-                "t2.large",
-                "t2.xlarge",
-                "t2.2xlarge",
-                "m4.large",
-                "m4.xlarge",
-                "m4.2xlarge",
-                "m4.4xlarge",
-                "m4.10xlarge",
-                "m4.16xlarge",
-                "m5.large",
-                "m5.xlarge",
-                "m5.2xlarge",
-                "m5.4xlarge",
-                "m5.12xlarge",
-                "m5.24xlarge",
-                "m5d.large",
-                "m5d.xlarge",
-                "m5d.2xlarge",
-                "m5d.4xlarge",
-                "m5d.12xlarge",
-                "m5d.24xlarge",
-                # Current generation compute optimized
-                "c4",
-                "c5",
-                "c5d",
-                "c4.large",
-                "c4.xlarge",
-                "c4.2xlarge",
-                "c4.4xlarge",
-                "c4.8xlarge",
-                "c5.large",
-                "c5.xlarge",
-                "c5.2xlarge",
-                "c5.4xlarge",
-                "c5.9xlarge",
-                "c5.18xlarge",
-                "c5d.xlarge",
-                "c5d.2xlarge",
-                "c5d.4xlarge",
-                "c5d.9xlarge",
-                "c5d.18xlarge",
-                # Current generation memory optimized
-                "r4",
-                "x1",
-                "x1e",
-                "r4.large",
-                "r4.xlarge",
-                "r4.2xlarge",
-                "r4.4xlarge",
-                "r4.8xlarge",
-                "r4.16xlarge",
-                "x1.16xlarge",
-                "x1.32xlarge",
-                "x1e.xlarge",
-                "x1e.2xlarge",
-                "x1e.4xlarge",
-                "x1e.8xlarge",
-                "x1e.16xlarge",
-                "x1e.32xlarge",
-                # Current generation storage optimized
-                "d2",
-                "h1",
-                "i3",
-                "d2.xlarge",
-                "d2.2xlarge",
-                "d2.4xlarge",
-                "d2.8xlarge",
-                "h1.2xlarge",
-                "h1.4xlarge",
-                "h1.8xlarge",
-                "h1.16xlarge",
-                "i3.large",
-                "i3.xlarge",
-                "i3.2xlarge",
-                "i3.4xlarge",
-                "i3.8xlarge",
-                "i3.16xlarge",
-                "i3.metal",
-                # Current generation accelerated computing
-                "f1",
-                "g3",
-                "p2",
-                "p3",
-                "f1.2xlarge",
-                "f1.16xlarge",
-                "g3.4xlarge",
-                "g3.8xlarge",
-                "g3.16xlarge",
-                "p2.xlarge",
-                "p2.8xlarge",
-                "p2.16xlarge",
-                "p3.2xlarge",
-                "p3.8xlarge",
-                "p3.16xlarge",
-                # Previous generation general purpose
-                "m1",
-                "m3",
-                "m1.small",
-                "m1.medium",
-                "m1.large",
-                "m1.xlarge",
-                "m3.medium",
-                "m3.large",
-                "m3.xlarge",
-                "m3.2xlarge",
-                # Previous generation compute optimized
-                "c1",
-                "cc2",
-                "c3",
-                "c1.medium",
-                "c1.xlarge",
-                "cc2.8xlarge",
-                "c3.large",
-                "c3.xlarge",
-                "c3.2xlarge",
-                "c3.4xlarge",
-                "c3.8xlarge",
-                # Previous generation memory optimized
-                "m2",
-                "cr1",
-                "r3",
-                "m2.xlarge",
-                "m2.2xlarge",
-                "m2.4xlarge",
-                "cr1.8xlarge",
-                "r3.large",
-                "r3.xlarge",
-                "r3.2xlarge",
-                "r3.4xlarge",
-                "r3.8xlarge",
-                # Previous generation storage optimized
-                "hs1",
-                "i2",
-                "hs1.8xlarge",
-                "i2.xlarge",
-                "i2.2xlarge",
-                "i2.4xlarge",
-                "i2.8xlarge",
-                # Previous generation GPU optimized
-                "g2",
-                "g2.2xlarge",
-                "g2.8xlarge",
-                # Previous generation micro
-                "t1.micro",
-            }
+            valid_instance_types = set(["optimal"]).union(_ec2_instance_types())
 
             if not set(instance_types) < valid_instance_types:
                 raise aws.CloudknotInputError(
